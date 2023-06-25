788a57af3d6e04ff22c88477fce7f438238d8973
Erwan Hervé <erwan.herve@outlook.fr>
2023-06-25 15:40:44 +0200

    v2

--- convert.ps1
+++ convert.ps1
@@ -4,7 +4,6 @@
 # Sinon la carte graphique est une Nvidia Pascal/Turing/Ampere (1/2/3XXX) (modèle commençant par 60 ou 70), le codec utilisé sera h265. (Pour la génération Pascal les B-frames ne sont pas compatible)
 # Sinon le codec utilisé sera h264.
 # Les fichiers déjà réencodés avec ces codecs ou avec HandBrake HEVC ne seront pas pris en compte.
-# Le bitrate utilisé par défaut pour le réencodage sera la moitié du bitrate original, arrondi à l'entier supérieur.
 # Exemple d'utilisation :
 # .\reencode-video.ps1
 # .\reencode-video.ps1 C:\Videos\MaVideo.mp4
@@ -12,24 +11,47 @@
 # Définition d'un paramètre en entrée $FilePath de type string. Si aucune valeur n'est passée, il prend la valeur $null.
 param (
     [string]$FilePath = $null,
-    [int]$overridebitrate = $null
+    [int]$qualité,
+    [switch]$avc,
+    [switch]$hevc,
+    [switch]$av1
 )
-# Définition de trois variables qui contiennent les chaînes de caractères "av1_nvenc", "hevc_nvenc" et "h264_nvenc".
-$av1="av1_nvenc"
-$hevc="hevc_nvenc"
-$avc="h264_nvenc"
-Import-Module CIM -ErrorAction SilentlyContinue
-$gpu = Get-CimInstance -ClassName CIM_VideoController | Select-Object -ExpandProperty Name
-if ($gpu -match ".*4\d\d\d$")
-{
-    $nvenc= $av1
+if ($qualité -gt 51) {
+    Write-Host "Le paramètre qualité ne doit pas dépasser 51." -ForegroundColor Magenta
+    $qualité = 51
 }
-elseif ($gpu -match ".*[1-4]\d\d\d$")
-{
-    $nvenc= $hevc
+if ($qualité) {
+    Write-Host "La qualité choisie est $qualité" -ForegroundColor DarkGray
 }
 else {
-    $nvenc= $avc
+    $qualité=28
+    Write-Host "Paramètre -qualité non renseigné, valeur par défaut : $qualité" -ForegroundColor DarkGray
+}
+# Définition de trois variables qui contiennent les chaînes de caractères "av1_nvenc", "hevc_nvenc" et "h264_nvenc".
+$av1_nvenc="av1_nvenc"
+$hevc_nvenc="hevc_nvenc"
+$avc_nvenc="h264_nvenc"
+Import-Module CIM -ErrorAction SilentlyContinue
+$gpu = Get-CimInstance -ClassName CIM_VideoController | Select-Object -ExpandProperty Name
+if (!$av1 -and !$hevc -and !$avc)
+{
+    Write-Host "Aucun codec choisi, il sera determiné en fonction du GPU" -ForegroundColor DarkGray
+    $codec = $false
+}
+if (($gpu -match ".*[4-9]\d\d\d$" -and !$codec) -or $av1)
+{
+    Write-Host "Le codec choisi est AV1" -ForegroundColor DarkGray
+    $codec=$av1_nvenc
+}
+elseif (($gpu -match ".*[2-3]\d\d\d$" -and !$codec) -or $hevc)
+{
+    Write-Host "Le codec choisi est HEVC (H265)" -ForegroundColor DarkGray
+    $codec=$hevc_nvenc
+}
+else
+{
+    Write-Host "Le codec choisi est AVC (H264)" -ForegroundColor DarkGray
+    $codec=$avc_nvenc
 }
 
 if (!$FilePath) {
@@ -40,7 +62,7 @@ if (!$FilePath) {
     $files = Get-ChildItem $folderPath -Recurse
 
     # Filtrer les fichiers en ne gardant que les fichiers vidéo et pour ne pas réencoder en boucle les mêmes fichiers
-    $videoFiles = $files | Where-Object { ($_.Extension -eq ".mp4" -or $_.Extension -eq ".avi" -or $_.Extension -eq ".mkv") -and $_.BaseName -notlike "*_$av1" -and $_.BaseName -notlike "*HANDBRAKE HEVC*" -and $_.BaseName -notlike "*_av1" -and $_.BaseName -notlike "*_$hevc" -and $_.BaseName -notlike "*_$avc"}
+    $videoFiles = $files | Where-Object { ($_.Extension -eq ".mp4" -or $_.Extension -eq ".avi" -or $_.Extension -eq ".mkv") -and $_.BaseName -notlike "*_$av1_nvenc*" -and $_.BaseName -notlike "*HANDBRAKE HEVC*" -and $_.BaseName -notlike "*HEVC*"-and $_.BaseName -notlike "*_av1" -and $_.BaseName -notlike "*_$hevc_nvenc*" -and $_.BaseName -notlike "*_$avc_nvenc*"}
 }
 else {
     # Obtenir le chemin du fichier passé en paramètre
@@ -51,40 +73,16 @@ else {
 }
 if (!$videoFiles)
 {
-    Write-Host "Aucun fichier vidéo ne correspond aux filtres"
+    Write-Host "Aucun fichier vidéo ne correspond aux filtres" -ForegroundColor Red
 }
 else
 {
     # Convertir les fichiers vidéo
-    Write-Output "Les vidéos qui vont être réencodés sont : " $videoFiles | Format-Table
-    $videoFiles | Foreach-Object {
-        $oldbitrate = ffmpeg -hide_banner -i "$($_.fullname)" -f null /dev/null - 2>&1 | ForEach-Object { if ($_ -match "bitrate: (\d+)") { Write-Output $Matches[1] } }
-        if ($oldbitrate -ge 55000) {
-            $divide= 4.5
-        }
-        elseif ($oldbitrate -ge 45000) {
-            $divide= 4
-        }
-        elseif ($oldbitrate -ge 35000) {
-            $divide= 3.5
-        }
-        elseif ($oldbitrate -ge 25000) {
-            $divide= 3
-        }
-        elseif ($oldbitrate -ge 15000) {
-            $divide= 2.5
-        }
-        else {
-            $divide= 2
-        }
-        if (!$overridebitrate)
-        {
-            $bitrate = [math]::Ceiling($oldbitrate / $divide)
-        }
-        else {
-            $bitrate = $overridebitrate
-        }
-        write-output "Réencondage en cours : $($_.Name) en $($nvenc) $($bitrate)KB/s au lieu de $($oldbitrate)KB/s"
-        Start-Process -NoNewWindow -Wait -FilePath "ffmpeg.exe" -ArgumentList "-hide_banner -i `"$($_.fullname)`" -c:a copy -preset slow -b_ref_mode middle -temporal-aq 1 -rc-lookahead 35 -spatial-aq 1 -c:v $($nvenc) -y -b:v $($bitrate)k -crf 24 `"$($_.directory)\$($_.basename)_$($nvenc).mp4`""
-    }
+    Write-Host -ForegroundColor Yellow "Les vidéos qui vont être réencodés sont : "
+    Write-Host -ForegroundColor Green ($videoFiles).Name | Format-Table
+     $videoFiles | Foreach-Object {
+        $cq=$qualité
+        write-host "Réencondage en cours : $($_.Name) avec le codec $($codec) en qualité $cq" -ForegroundColor Blue
+        Start-Process -NoNewWindow -Wait -FilePath "ffmpeg.exe" -ArgumentList "-hide_banner -i `"$($_.fullname)`" -c:a copy -c:s copy  -preset:v 1  -b_ref_mode 2  -temporal-aq 1  -rc-lookahead 35  -spatial-aq 1  -multipass 2  -c:v $($codec) -2pass true -y -crf 30 -loglevel error -stats -cq:v $cq -nonref_p 1 -f mp4 `"$($_.directory)\$($_.basename)_$($codec)_$cq.mp4`""
+     }
 }
